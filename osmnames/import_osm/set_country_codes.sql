2c6635002cc17202db1a57a6c4de65d728a70103
Philipp Koster <philipp.koster@startmail.com>
2017-04-28 17:44:39 +0200

    set country_code based on most intersecting country

--- osmnames/import_osm/set_country_codes.sql
+++ osmnames/import_osm/set_country_codes.sql
@@ -1,11 +1,15 @@
-DROP FUNCTION IF EXISTS set_country_code_for_polygons_within_geometry(VARCHAR(2), geometry);
-CREATE FUNCTION set_country_code_for_polygons_within_geometry(country_code_in VARCHAR(2), geometry_in GEOMETRY) RETURNS VOID AS $$
+DROP FUNCTION IF EXISTS get_country_code_by_geometry(geometry);
+CREATE FUNCTION get_country_code_by_geometry(geometry_in GEOMETRY) RETURNS VARCHAR(2) AS $$
 BEGIN
-  UPDATE osm_polygon SET country_code = country_code_in WHERE country_code = '' IS NOT FALSE
-                                                              AND place_rank = 4
-                                                              AND st_contains(geometry_in, geometry);
+  RETURN(
+    SELECT lower(country_code)
+    FROM country_osm_grid
+    WHERE st_intersects(geometry, geometry_in)
+    ORDER BY st_area(st_intersection(geometry, geometry_in)) DESC
+    LIMIT 1
+  );
 END;
-$$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql IMMUTABLE;
 
 
 DO $$
@@ -13,10 +17,9 @@ BEGIN
   -- use imported country code for polygons if present
   UPDATE osm_polygon SET country_code = lower(imported_country_code) WHERE imported_country_code IS NOT NULL;
 
-  -- use country grid to set country codes for containing elements
-  PERFORM set_country_code_for_polygons_within_geometry(lower(country_code), geometry)
-          FROM country_osm_grid
-          ORDER BY area ASC;
+  -- set missing country codes for every polygon with place_rank <= 4 (countries)
+  UPDATE osm_polygon SET country_code = get_country_code_by_geometry(geometry) WHERE country_code = '' IS NOT FALSE
+                                                                                     AND place_rank <= 4;
 
   CREATE INDEX IF NOT EXISTS idx_osm_polygon_country_code ON osm_polygon(country_code);
 END
--- tests/import_osm/test_set_country_codes.py
+++ tests/import_osm/test_set_country_codes.py
@@ -36,6 +36,37 @@ def test_osm_polygon_country_code_get_set_base_on_country_grid(session, schema, 
     assert session.query(tables.osm_polygon).get(1).country_code == 'ch'
 
 
+def test_osm_polygon_country_code_get_set_with_most_intersecting_country(session, schema, tables):
+    session.add(
+            tables.country_osm_grid(
+                country_code='CH',
+                geometry=WKTElement("POLYGON((0 0,4 0,4 1,0 1,0 0))", srid=3857)
+            )
+        )
+
+    session.add(
+            tables.country_osm_grid(
+                country_code='DE',
+                geometry=WKTElement("POLYGON((0 0,4 0,4 4,0 4,0 0))", srid=3857)
+            )
+        )
+
+    session.add(
+            tables.osm_polygon(
+                id=1,
+                name="Some country with missing country_code",
+                place_rank=4,
+                geometry=WKTElement("POLYGON((0 0,4 0,4 4,0 4,0 0))", srid=3857)
+            )
+        )
+
+    session.commit()
+
+    set_country_codes()
+
+    assert session.query(tables.osm_polygon).get(1).country_code == 'de'
+
+
 def test_osm_polygon_country_code_get_set_bases_on_imported_country_code(session, schema, tables):
     session.add(
             tables.osm_polygon(
